09f2420b8e58deaba1c8c41a65e6b81c5b1c2367
eric <eric@ericjohannesson.com>
2025-10-15 22:20:43 +0200

    fixed test messages

--- implementations/ocaml/testing/test.sh
+++ implementations/ocaml/testing/test.sh
@@ -38,20 +38,20 @@ do
 
 done
 
-color "# nmm-ocaml check-xml-schema ../dtd/axml.dtd 1> /dev/null:"
+color "# nmm-ocaml check-xml-schema ../dtd/axml.dtd > /dev/null:"
 ../bin/nmm-ocaml check-xml-schema dtd/axml.dtd > /dev/null
-color "# nmm-ocaml check-xml-schema ../dtd/exml.dtd 1> /dev/null:"
+color "# nmm-ocaml check-xml-schema ../dtd/exml.dtd > /dev/null:"
 ../bin/nmm-ocaml check-xml-schema dtd/exml.dtd > /dev/null
 
 for file in $(ls output/*.xml)
 do
-	color "# nmm-ocaml validate-xml ../dtd/axml.dtd cr_doc $file 1> /dev/null:"
+	color "# nmm-ocaml validate-xml ../dtd/axml.dtd cr_doc $file > /dev/null:"
 	../bin/nmm-ocaml validate-xml dtd/axml.dtd cr_doc $file > /dev/null
 done
 
 for file in $(ls input/*.xml)
 do
-	color "# nmm-ocaml validate-xml ../dtd/axml.dtd cr_doc $file 1> /dev/null:"
+	color "# nmm-ocaml validate-xml ../dtd/axml.dtd cr_doc $file > /dev/null:"
 	../bin/nmm-ocaml validate-xml dtd/axml.dtd cr_doc $file > /dev/null
 done
 
