94deae4993e9b6c35888443d8ff2e7ba521d2d85
Anders Lundstedt <git@anderslundstedt.se>
2025-10-24 04:56:50 +0200

    test nmm2xml: check xml files' conformity to schema

--- test-mercury-nmm2xml.sh
+++ test-mercury-nmm2xml.sh
@@ -3,15 +3,37 @@
 cd "$(dirname "$0")"
 exit_code=0
 for nmm_file in ./test-data-nmm2xml/*.nmm; do
-    nmm_mercury_output_file="$(mktemp)"
     expected_output_file="${nmm_file%.nmm}.xml"
+    ./bin/nmm-ocaml validate-xml ./specification/AST.dtd "$expected_output_file" > /dev/null 2>&1
+    if [[ "$?" -ne 0 ]]; then
+        >&2 echo "$expected_output_file does not conform to schema"
+        >&2 echo "  ./specification/AST.dtd"
+        >&2 echo "run"
+        >&2 echo "  ./bin/nmm-ocaml validate-xml ./specification/AST.dtd $expected_output_file"
+        >&2 echo "for further debugging"
+        exit_code=1
+        continue
+    fi
+    nmm_mercury_output_file="$(mktemp)"
     ./bin/nmm-mercury nmm2xml "$nmm_file" > "$nmm_mercury_output_file"
+    ./bin/nmm-ocaml validate-xml ./specification/AST.dtd "$nmm_mercury_output_file" > /dev/null 2>&1
+    if [[ "$?" -ne 0 ]]; then
+        >&2 echo "output from"
+        >&2 echo "  nmm-mercury nmm2xml $nmm_file"
+        >&2 echo "does not conform to schema"
+        >&2 echo "  ./specification/AST.dtd"
+        >&2 echo "run"
+        >&2 echo "  ./bin/nmm-ocaml validate-xml ./specification/AST.dtd $nmm_mercury_output_file"
+        >&2 echo "for further debugging"
+        exit_code=1
+        continue
+    fi
     xmldiff --check --ratio-mode faster "$nmm_mercury_output_file" "$expected_output_file" > /dev/null
     if [[ "$?" -ne 0 ]]; then
-        echo "output from"
-        echo "  nmm-mercury nmm2xml $nmm_file"
-        echo "differs from expected output in"
-        echo "  $expected_output_file"
+        >&2 echo "output from"
+        >&2 echo "  nmm-mercury nmm2xml $nmm_file"
+        >&2 echo "differs from expected output in"
+        >&2 echo "  $expected_output_file"
         exit_code=1
     fi
 done
