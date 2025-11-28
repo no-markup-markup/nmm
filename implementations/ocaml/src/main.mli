ce2d6db49ba5abbff146dbffc955c7ff0f4fef4c
eric <eric@ericjohannesson.com>
2025-11-28 02:10:43 +0100

    html semantics: updated documentation

--- implementations/ocaml/src/main.mli
+++ implementations/ocaml/src/main.mli
@@ -20,11 +20,18 @@ evaluates to
 {!val:Compiler_of_doc.txt_of_tr_doc}[ doc]
 *)
 
+
+val default_css : string
+(**
+evaluates to 
+{!val:Html_utils.internal_css} ["6ch" "8rem"]
+*)
+
 val html_of_doc : string option -> string option -> Doc_types.tr_doc -> string
 (**
 {[html_of_doc None None doc]} evaluates to a string containing a html-document with an internal css stylesheet, specified by the value of
 
-["<style>" ^ (]{!val:Html_utils.internal_css} [ ] {!val:Common_utils.doc_settings}[) ^ "</style>"]
+["<style>" ^ default_css ^ "</style>"]
 
 The body of the html-document is specified by the value of
 
@@ -113,4 +120,4 @@ val validate_xml : string -> string -> string
 first calls the {{:https://github.com/ncannasse/xml-light}Xml-light} function [Dtd.parse_file] for parsing xml-schema.dtd, and [Dtd.check] for checking that it is well-defined. It then calls the function {!val:Xml_right.parse_file} for parsing xml-file.xml into an object of the {{:https://github.com/ncannasse/xml-light}Xml-light} type [Xml.xml]. Lastly, it calls [Dtd.prove] for proving (or disproving) that xml-file.xml is an instance of xml-schema.dtd.
 *)
 
-val default_css : string
+
