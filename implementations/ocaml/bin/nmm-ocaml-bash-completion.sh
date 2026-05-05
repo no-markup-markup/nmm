679a46407e6b4516246f3fb1428aac930c410e3a
eric <eric@ericjohannesson.com>
2026-05-06 01:02:53 +0200

    updated bash completion script for nmm-ocaml

--- implementations/ocaml/bin/nmm-ocaml-bash-completion.sh
+++ implementations/ocaml/bin/nmm-ocaml-bash-completion.sh
@@ -1,42 +1,55 @@
 # bash completion for nmm-ocaml (and for wrapper scripts txt-of-nmm, html-of-nmm, and pdf-of-nmm)
 
-# either source this file in ~/.bash_completion by appending the line 'source <path-to-this-file>',
-# or copy it to /usr/share/bash-completion/completions/nmm-ocaml
+# to enable bash completion for nmm-ocaml, you can either
+# 1) copy this file to /usr/share/bash-completion/completions/nmm-ocaml, or
+# 2) append the line 'source <path-to-this-file>' to ~/.bash_completion
 
-subcommands='html-of-nmm txt-of-nmm exml-of-nmm axml-of-nmm html-of-axml txt-of-axml exml-of-axml'
-axml_options='--tags'
-exml_options="$axml_options --quiet --numbering --allow-custom-numbering"
-txt_options="$exml_options --margin --width"
-html_options="$exml_options --margin --css"
-numbering_options='a1i ai1 1ai 1ia ia1 i1a'
-margin_options=$(echo $(seq 0 100))
-width_options=$(echo $(seq 1 100))
+_nmm_ocaml_get_options () {
 
-get_options () {
+	local subcommands='html-of-nmm txt-of-nmm exml-of-nmm axml-of-nmm html-of-axml txt-of-axml exml-of-axml'
+	local axml_options='--tags'
+	local exml_options="$axml_options --quiet --numbering --allow-custom-numbering"
+	local txt_options="$exml_options --margin --width"
+	local html_options="$exml_options --margin --css --lang"
+	local numbering_options='a1i ai1 1ai 1ia ia1 i1a'
+	local lang_options='en sv fr de es' #etc
+
 	case $1 in
 		nmm-ocaml )
-			echo $subcommands ;;
+			echo $subcommands
+			;;
 		axml-of-* )
-			echo $axml_options ;;
+			echo $axml_options
+			;;
 		exml-of-* )
-			echo $exml_options ;;
+			echo $exml_options
+			;;
 		txt-of-* )
-			echo $txt_options ;;
+			echo $txt_options
+			;;
 		html-of-* | pdf-of-* )
-			echo $html_options ;;
+			echo $html_options
+			;;
 		--numbering )
-			echo $numbering_options ;;
+			echo $numbering_options
+			;;
+		--lang )
+			echo $lang_options
+			;;
 		--margin )
-			echo $margin_options ;;
+			echo $(seq 0 100) #etc
+			;;
 		--width )
-			echo $width_options ;;
+			echo $(seq 1 100) #etc
+			;;
 		*)
-			echo '' ;;
+			echo ''
+			;;
 	esac
 
 }
 
-actual_subcommand=''
+_nmm_ocaml_chosen_subcommand=''
 
 _nmm_ocaml () {
 
@@ -44,31 +57,34 @@ _nmm_ocaml () {
 	COMPREPLY=()
 	cur=${COMP_WORDS[COMP_CWORD]}
 	prev=${COMP_WORDS[COMP_CWORD-1]}
-	options=$(get_options $prev)
+	options=$(_nmm_ocaml_get_options $prev)
 
 	case $prev in
 		nmm-ocaml )
-			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) ) ;;
+			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) )
+			;;
 		*-of-* )
-			actual_subcommand=$prev
-			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) $(compgen -f -- ${cur}) ) ;;
-		--numbering )
-			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) ) ;;
-		--margin )
-			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) ) ;;
+			_nmm_ocaml_chosen_subcommand=$prev
+			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) $(compgen -f -- ${cur}) )
+			;;
+		--numbering | --margin | --width | --lang )
+			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) )
+			;;
 		--tags | --css )
-			COMPREPLY=( $(compgen -f -- ${cur}) ) ;;
+			COMPREPLY=( $(compgen -f -- ${cur}) )
+			;;
 		- )
-			COMPREPLY=() ;;
+			COMPREPLY=()
+			;;
 		* )
-			options=$(get_options $actual_subcommand)
-			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) $(compgen -f -- ${cur}) ) ;;
+			options=$(_nmm_ocaml_get_options $_nmm_ocaml_chosen_subcommand)
+			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) $(compgen -f -- ${cur}) )
+			;;
 	esac
 
 	return 0
 }
 
 complete -o nospace -F _nmm_ocaml nmm-ocaml html-of-nmm txt-of-nmm pdf-of-nmm
-
 
 
