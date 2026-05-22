215d80bb89fc388ece6226c33ac1166eb401af08
eric <eric@ericjohannesson.com>
2026-05-22 13:56:21 +0200

    improved error handling in nmm-ocaml wrapper scripts

--- implementations/ocaml/bin/html-of-nmm.sh
+++ implementations/ocaml/bin/html-of-nmm.sh
@@ -1,11 +1,30 @@
 #! /usr/bin/env bash
 
-insert_images() {
-  sed 's/\&lt;\([^ ]\+\).\(jpg\|png\|svg\|webp\|gif\)\&gt;/<img class="inserted_image" alt="Image source is \1.\2" src="\1.\2">/g' $1
+set -eo pipefail
+
+_nmm_ocaml_insert_images() {
+	sed 's/\&lt;\([^ ]\+\).\(jpg\|png\|svg\|webp\|gif\)\&gt;/<img class="inserted_image" alt="Image source is \1.\2" src="\1.\2">/g' $1
 }
 
-insert_links() {
-  sed 's/\&lt;\(https\?:\/\/\)\([^ ]\+\)\&gt;/<a class="inserted_link" href="\1\2">\2<\/a>/g' $1
+_nmm_ocaml_insert_links() {
+	sed 's/\&lt;\(https\?:\/\/\)\([^ ]\+\)\&gt;/<a class="inserted_link" href="\1\2">\2<\/a>/g' $1
 }
 
-nmm-ocaml html-of-nmm $@ | insert_images | insert_links > $(basename -s .nmm ${@: -1}).html
+_nmm_ocaml_make_html () {
+	local temp=$(mktemp)
+	nmm-ocaml html-of-nmm $@ | _nmm_ocaml_insert_images | _nmm_ocaml_insert_links > $temp
+	local exit_code=$?
+
+	case $exit_code in
+		0)
+			mv $temp $(basename -s .nmm ${@: -1}).html
+			;;
+		*)
+			rm $temp
+			return $exit_code
+			;;
+	esac
+}
+
+_nmm_ocaml_make_html $@
+
--- implementations/ocaml/bin/txt-of-nmm.sh
+++ implementations/ocaml/bin/txt-of-nmm.sh
@@ -1,3 +1,22 @@
 #! /usr/bin/env bash
 
-nmm-ocaml txt-of-nmm $@ > $(basename -s .nmm ${@: -1}).txt
+set -eo pipefail
+
+_nmm_ocaml_make_txt () {
+	local temp=$(mktemp)
+	nmm-ocaml txt-of-nmm $@ > $temp
+	local exit_code=$?
+
+	case $exit_code in
+		0)
+			mv $temp $(basename -s .nmm ${@: -1}).txt
+			;;
+		*)
+			rm $temp
+			return $exit_code
+			;;
+	esac
+}
+
+_nmm_ocaml_make_txt $@
+
