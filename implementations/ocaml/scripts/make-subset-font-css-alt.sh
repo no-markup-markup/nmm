6005dc5cc3bfd1d591123c778145cb8d2986e486
eric <eric@ericjohannesson.com>
2026-06-10 02:59:31 +0200

    added alternative script for making subset fonts that does not compile nmm to txt first

---
+++ implementations/ocaml/scripts/make-subset-font-css-alt.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+make_subset_font_css_main () {
+	local input_ttf_file=$1
+	local input_nmm_file=$2
+	local temp_ttf_file=$(mktemp)
+	local ascii_unicodes=$(printf '%04x ' $(seq 0 127))
+	local unsorted_unicodes=$(iconv -f utf8 -t utf32 ${input_nmm_file} | hexdump -v -e '"%04x "')
+	local unicodes=$(echo "${ascii_unicodes} ${unsorted_unicodes}" | sort -u -t ' ')
+	pyftsubset ${input_ttf_file} --unicodes="${unicodes}" --output-file="${temp_ttf_file}"
+	local output_font_base64=$(base64 --wrap=0 ${temp_ttf_file})
+	rm ${temp_ttf_file}
+	echo "
+@font-face {
+  font-family: custom-subset;
+  src: url(data:font/ttf;base64,${output_font_base64}) format('truetype');
+}
+
+html {
+  font-family : custom-subset;
+}
+"
+}
+
+
+if [ $# -ne 2 ]
+then
+	echo "USAGE:"
+	echo "  make-subset-font-css <path-to-ttf-file> <path-to-nmm-file>"
+	exit 1
+else
+	make_subset_font_css_main $1 $2
+fi
